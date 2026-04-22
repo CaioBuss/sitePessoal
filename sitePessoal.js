6f8c70c98e6d55d07638284d2957cdaf83d80766
Eivor Buss <caiofdab@gmail.com>
2026-04-22 19:12:37 -0300

    Faltou a parte do JavaScript

--- sitePessoal.js
+++ sitePessoal.js
@@ -1,7 +1,28 @@
 function toggleDarkMode() {
-    document.body.classList.toggle("dark");
+    document.body.classList.toggle("light");
+    if (document.body.classList.contains("light")) {
+        localStorage.setItem("theme", "light");
+    } else {
+        localStorage.setItem("theme", "dark");
+    }
 }
+window.onload =() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (theme === "light") {
+        document.body.classList.add("light");
+    }
 
-function toggleSkills() {
-    const skillsList = document.getElementById("Skills");
+    revealOnScroll();
+}
+function revealOnScroll() {
+    const elements = document.querySelectorAll(".reveal");
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (CustomElementRegistry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    });
+
+    elements.forEach(e1 => observer.observe(e1));
 }
